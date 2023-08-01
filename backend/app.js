2857cf0e5fe624de07dc3499801d60663fd9c7da
Anastasia Gorlina <gobi199@yandex.ru>
2023-08-01 08:44:48 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
 app.use('/', userRoutes); // запускаем импортированные роуты
 app.use('/', cardRoutes); // запускаем импортированные роуты
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
